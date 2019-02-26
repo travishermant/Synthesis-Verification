0ad63d8f25f9e3085a3dafa341ac8226a69d2e98
Travis Hermant <43862625+travishermant@users.noreply.github.com>
2019-02-25 19:17:28 -0800

    Create S_RTL_3.sv

---
+++ S_RTL_3.sv
@@ -0,0 +1,36 @@
+/*
+ECE481 Group 2
+2/25/2019
+Counter with 2 clocks, synthesized with one clock mux
+Provided by Dr. Ataur Patwary
+*/
+
+
+module sync_counter(
+	input logic clkA, clkB,
+	input logic rst,
+	input logic cntrl,
+	input logic di,
+	output logic dco);
+	
+logic clkAB, clkCDl;
+logic dao;
+logic dbo;
+
+always_comb
+begin
+	clkAB = cntrl ? clkB : clkA;
+	dbo = dao | di;
+end
+
+always_ff @ (posedge clkAB)
+begin
+	if(rst)
+	dco <= 1'b0;
+	
+	else
+	begin
+		dco <= dbo;
+	end
+end
+endmodule
