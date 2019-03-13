cf8c1b29b20d4379c019ec6ea9c9793e84af99cb
Travis Hermant <43862625+travishermant@users.noreply.github.com>
2019-03-13 14:29:36 -0700

    No errors on synthesis

---
+++ 3_13_2019/S_RTL_1/work/S_RTL_1_netlist.v
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////
+// Created by: Synopsys DC Expert(TM) in wire load mode
+// Version   : L-2016.03-SP3
+// Date      : Tue Mar 12 18:36:23 2019
+/////////////////////////////////////////////////////////////
+
+
+module sync_counter ( fast_clk, data, multiply );
+  input [3:0] data;
+  output [3:0] multiply;
+  input fast_clk;
+  wire   \count[1] , \decode[2] , N1, n1, n2, n4, n5, n6, n7, n8, n9, n10, n11,
+         n12, n13, n14, n15, n16, n17, n18, n19, n20;
+  wire   [3:0] synch;
+  wire   [3:0] comb1;
+  wire   [3:0] comb2;
+
+  DFFX1_RVT \shift_reg[3]  ( .D(data[3]), .CLK(fast_clk), .Q(n9) );
+  DFFX1_RVT \shift_reg[2]  ( .D(data[2]), .CLK(fast_clk), .Q(n8) );
+  DFFX1_RVT \shift_reg[1]  ( .D(data[1]), .CLK(fast_clk), .Q(n7) );
+  DFFX1_RVT \shift_reg[0]  ( .D(data[0]), .CLK(fast_clk), .Q(n10) );
+  DFFX1_RVT \count_reg[0]  ( .D(n16), .CLK(fast_clk), .QN(n16) );
+  DFFX1_RVT \count_reg[1]  ( .D(N1), .CLK(fast_clk), .Q(\count[1] ) );
+  DFFX1_RVT \decode_reg[3]  ( .D(comb1[3]), .CLK(n15), .QN(n5) );
+  DFFX1_RVT \decode_reg[2]  ( .D(comb1[2]), .CLK(n20), .Q(\decode[2] ) );
+  DFFX1_RVT \decode_reg[1]  ( .D(comb1[1]), .CLK(n20), .Q(n17), .QN(n6) );
+  DFFX1_RVT \decode_reg[0]  ( .D(comb1[0]), .CLK(n20), .Q(n18), .QN(n4) );
+  DFFX1_RVT \multiply_reg[3]  ( .D(comb2[3]), .CLK(n15), .Q(multiply[3]) );
+  DFFX1_RVT \multiply_reg[2]  ( .D(comb2[2]), .CLK(n20), .Q(multiply[2]) );
+  DFFX1_RVT \multiply_reg[1]  ( .D(comb2[1]), .CLK(n20), .Q(multiply[1]) );
+  DFFX1_RVT \multiply_reg[0]  ( .D(comb2[0]), .CLK(n20), .Q(multiply[0]) );
+  DFFX1_RVT \synch_reg[0]  ( .D(n14), .CLK(fast_clk), .Q(synch[0]) );
+  DFFX1_RVT \synch_reg[3]  ( .D(n13), .CLK(fast_clk), .Q(synch[3]) );
+  DFFX1_RVT \synch_reg[2]  ( .D(n12), .CLK(fast_clk), .Q(synch[2]) );
+  DFFX1_RVT \synch_reg[1]  ( .D(n11), .CLK(fast_clk), .Q(synch[1]) );
+  AO22X1_RVT U3 ( .A1(synch[3]), .A2(n1), .A3(n9), .A4(n19), .Y(n13) );
+  AO22X1_RVT U4 ( .A1(synch[2]), .A2(n1), .A3(n8), .A4(n19), .Y(n12) );
+  AO22X1_RVT U5 ( .A1(synch[1]), .A2(n1), .A3(n7), .A4(n2), .Y(n11) );
+  AO22X1_RVT U6 ( .A1(synch[0]), .A2(n1), .A3(n10), .A4(n2), .Y(n14) );
+  INVX0_RVT U7 ( .A(n1), .Y(n2) );
+  OR2X1_RVT U8 ( .A1(n15), .A2(n16), .Y(n1) );
+  OR2X1_RVT U15 ( .A1(synch[3]), .A2(synch[1]), .Y(comb1[3]) );
+  AND2X1_RVT U16 ( .A1(synch[1]), .A2(synch[2]), .Y(comb1[2]) );
+  OR2X1_RVT U17 ( .A1(synch[1]), .A2(synch[0]), .Y(comb1[1]) );
+  AND2X1_RVT U18 ( .A1(synch[0]), .A2(synch[1]), .Y(comb1[0]) );
+  XNOR2X1_RVT U19 ( .A1(\count[1] ), .A2(n16), .Y(N1) );
+  NBUFFX2_RVT U20 ( .A(n2), .Y(n19) );
+  INVX1_RVT U21 ( .A(\count[1] ), .Y(n20) );
+  INVX1_RVT U22 ( .A(\count[1] ), .Y(n15) );
+  AND2X1_RVT U23 ( .A1(n17), .A2(n18), .Y(comb2[0]) );
+  NAND2X0_RVT U24 ( .A1(n4), .A2(n6), .Y(comb2[1]) );
+  AND2X1_RVT U25 ( .A1(\decode[2] ), .A2(n17), .Y(comb2[2]) );
+  NAND2X0_RVT U26 ( .A1(n5), .A2(n6), .Y(comb2[3]) );
+endmodule
+
---
+++ 3_13_2019/S_RTL_8/scripts/S_RTL_8.tcl
@@ -0,0 +1,108 @@
+# To reset the earlier designs
+reset_design
+
+# For removing the all the design
+remove_design -designs
+
+# To check whether the previous design is removed
+current_design
+
+lappend search_path "/u/thermant/ECE_4_581_WINTER_2018/ECE_4_581_WINTER_2018/Final_Project/S_RTL_8/models"
+
+set search_path ""
+lappend search_path "/pkgs/synopsys/2016/libs/SAED32_EDK/lib/stdcell_hvt/db_nldm"
+lappend search_path "/pkgs/synopsys/2016/libs/SAED32_EDK/lib/stdcell_rvt/db_nldm"
+lappend search_path "/pkgs/synopsys/2016/libs/SAED32_EDK/lib/stdcell_lvt/db_nldm"
+lappend search_path "/pkgs/synopsys/2016/libs/SAED32_EDK/lib/io_std/db_nldm"
+lappend search_path "/pkgs/synopsys/2016/libs/SAED32_EDK/lib/sram/db_nldm"
+
+set synthetic_library dw_foundation.sldb
+set target_library "saed32lvt_ss0p75v125c.db saed32rvt_ss0p75v125c.db"
+set link_library "* saed32hvt_ss0p75v125c.db saed32lvt_ss0p75v125c.db saed32rvt_ss0p75v125c.db saed32io_wb_ss0p95v125c_2p25v.db \
+ saed32sram_ss0p95v125c.db dw_foundation.sldb"
+
+set target_library "saed32rvt_ff1p16v125c.db"
+set link_library   "saed32rvt_ff1p16v125c.db dw_foundation.sldb"
+
+
+
+# Analyzing the current design
+analyze -format sverilog /u/thermant/ECE_4_581_WINTER_2018/ECE_4_581_WINTER_2018/Final_Project/S_RTL_8/rtl/S_RTL_8.sv
+
+# Current design after anlayzing
+current_design
+
+# Elaborate the design
+elaborate sync_counter
+
+create_clock -name "clkA" -period 1.0 -waveform {0.0 0.5} clkA
+create_clock -name "clkB" -period 2.0 -waveform {0.0 1.0} clkB
+create_clock -name "clkC" -period 3.0 -waveform {0.0 1.5} clkC
+create_clock -name "clkD" -period 4.0 -waveform {0.0 2.0} clkD
+set_clock_groups -logically_exclusive -group {clkA clkB} -group {clkC clkD} -name "clkCOMB"
+remove_from_collection [all_inputs] {clkA clkB clkC clkD}
+set_max_area 9000
+
+set_input_delay 0.1 -clock [get_clocks clkA] [get_ports rst]
+set_input_delay 0.1 -clock [get_clocks clkA] [get_ports cntrlAB]
+set_input_delay 0.1 -clock [get_clocks clkA] [get_ports cntrlCD]
+set_input_delay 0.1 -clock [get_clocks clkA] [get_ports di]
+
+set_input_delay 0.1 -clock [get_clocks clkB] [get_ports rst]
+set_input_delay 0.1 -clock [get_clocks clkB] [get_ports cntrlAB]
+set_input_delay 0.1 -clock [get_clocks clkB] [get_ports cntrlCD]
+set_input_delay 0.1 -clock [get_clocks clkB] [get_ports di]
+
+set_input_delay 0.1 -clock [get_clocks clkC] [get_ports rst]
+set_input_delay 0.1 -clock [get_clocks clkC] [get_ports cntrlAB]
+set_input_delay 0.1 -clock [get_clocks clkC] [get_ports cntrlCD]
+set_input_delay 0.1 -clock [get_clocks clkC] [get_ports di]
+
+set_input_delay 0.1 -clock [get_clocks clkD] [get_ports rst]
+set_input_delay 0.1 -clock [get_clocks clkD] [get_ports cntrlAB]
+set_input_delay 0.1 -clock [get_clocks clkD] [get_ports cntrlCD]
+set_input_delay 0.1 -clock [get_clocks clkD] [get_ports di]
+
+set_output_delay 0.1 -clock [get_clocks clkA] [all_outputs]
+set_output_delay 0.1 -clock [get_clocks clkB] [all_outputs]
+set_output_delay 0.1 -clock [get_clocks clkC] [all_outputs]
+set_output_delay 0.1 -clock [get_clocks clkD] [all_outputs]
+# compile map the sequential cell exactly as in the rtl
+# compile -exact_map -ungroup_all
+compile -exact_map
+
+# Number of inputs
+all_inputs 
+set number_of_inputs [sizeof_collection [all_inputs]]
+puts "Number_of_inputs $number_of_inputs"
+
+# Number of outputs
+all_outputs
+set number_of_outputs [sizeof_collection [all_outputs ]]
+puts "Number_of_outputs $number_of_outputs"
+
+# Number of sequentials:
+all_registers
+set number_of_sequentials [sizeof_collection [all_registers]]
+puts "Number_of_sequentials $number_of_sequentials"
+
+report_cell
+
+report_qor -sig 3 > ../reports/qor.rpt
+
+report_qor > ../scripts/QOR_Report_File
+
+write_file -format verilog  -output  S_RTL_8_netlist.v
+
+report_area
+
+check_timing -include {no_driving_cell no_input_delay partial_input_delay unconstrained_endpoints } > ../reports/check_timing.rpt
+
+report_power > ../reports/power.rpt
+
+report_constraint -all_violators -verbose -sig 3 > ../reports/constraints.rpt
+# report_hierarchy
+
+# report_compile_options
+
+# write -format ddc -hierarchy -output FIFO.ddc
